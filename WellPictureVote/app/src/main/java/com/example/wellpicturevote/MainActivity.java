c89931a16f90198c7f252c680721f8bd9f1e6898
밥장군 <jangun@son-ogmus-MacBook-Air.local>
2022-03-22 15:05:19 +0900

    add voteRating.class

--- WellPictureVote/app/src/main/java/com/example/wellpicturevote/MainActivity.java
+++ WellPictureVote/app/src/main/java/com/example/wellpicturevote/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.wellpicturevote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     int[] voteCount;
     ImageView[] image;
+    Button btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +38,16 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     voteCount[index]++;
-                    Toast.makeText(MainActivity.this, imgName[index] + "번그림이 " + voteCount[index] + "번 눌러졌습니다.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, imgName[index] + "번이 " + voteCount[index] + "번 눌러졌습니다.", Toast.LENGTH_SHORT).show();
+                }
+            });
+            btnExit = (Button)findViewById(R.id.button01);
+            btnExit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intentHitCount = new Intent(getApplicationContext(), VoteResult.class);
+                    intentHitCount.putExtra("hc", voteCount);
+                    startActivity(intentHitCount);
                 }
             });
         }
---
+++ WellPictureVote/app/src/main/java/com/example/wellpicturevote/VoteResult.java
@@ -0,0 +1,38 @@
+package com.example.wellpicturevote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+
+public class VoteResult extends AppCompatActivity {
+
+    RatingBar[] rBar;
+    Button btnReturn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote_result);
+
+        Intent intentReceive = getIntent();
+        int[] hitCount = intentReceive.getIntArrayExtra("hc");
+        rBar = new RatingBar[9];
+        int[] rBarId = {R.id.ratingBar1, R.id.ratingBar2, R.id.ratingBar3, R.id.ratingBar4, R.id.ratingBar5, R.id.ratingBar6, R.id.ratingBar7, R.id.ratingBar8, R.id.ratingBar9};
+
+        for(int i=0; i<rBar.length; i++) {
+            rBar[i] = (RatingBar)findViewById(rBarId[i]);
+            rBar[i].setRating((float)hitCount[i]);
+        }
+
+        btnReturn = (Button)findViewById(R.id.buttonreturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
