c3260150ec654a9667be7991d6b59d78a8a9ded8
밥장군 <sonokmoo@gmail.com>
2022-05-14 08:54:21 +0900

    news app, Room test

--- newsapp/app/src/main/java/com/example/newsapp/RequestManager.java
+++ newsapp/app/src/main/java/com/example/newsapp/RequestManager.java
@@ -16,11 +16,13 @@ import retrofit2.http.Query;
 public class RequestManager {
     Context context;
 
+    // 뉴스 api를 가져올 기본 주소 설정.
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://newsapi.org/v2/")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 
+    // api의 기본 정보, 국가, 카테고리, 쿼리, context 정의. api의 기본 정보는 지정되어 있는 것만 사용할 수 있다.
     public void getNewsHeadLines(OnFetchDataListener listener, String category, String query)
     {
         CallNewsApi callNewsApi = retrofit.create(CallNewsApi.class);
@@ -31,7 +33,7 @@ public class RequestManager {
                 @Override
                 public void onResponse(Call<NewsApiResponse> call, Response<NewsApiResponse> response) {
                     if (!response.isSuccessful()) {
-                        Toast.makeText(context, "Error!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context, "context를 가져오는 동안 에러가 발생했습니다.!", Toast.LENGTH_SHORT).show();
                     }
 
                     listener.onFetchData(response.body().getArticles(), response.message());
@@ -40,7 +42,7 @@ public class RequestManager {
 
                 @Override
                 public void onFailure(Call<NewsApiResponse> call, Throwable t) {
-                    listener.onError("Request Failed!");
+                    listener.onError("요청을 실행할 수 없습니다.");
 
                 }
             });
@@ -54,6 +56,7 @@ public class RequestManager {
         this.context = context;
     }
 
+    // api의 기본 정보 정의.
     public interface CallNewsApi {
         @GET("top-headlines")
         Call<NewsApiResponse> callHeadline(
